f1d8b511d02f5dc90aa51eabdb7722ec15fbfebc
Sai68329 <saichaitanya68329@gmail.com>
2026-02-22 02:03:46 +0530

    small changes done

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
+app.use(cors());
 const statusRoutes = require("./routes/statusRoutes");
 const checkRoutes = require("./routes/checkRoutes");
 const competitorRoutes = require("./routes/competitorRoutes");
